ac48b9738163e3657748595bf583c2a10e57822d
Jari Aalto <jari.aalto@cante.net>
2024-04-06 20:33:36 +0300

    t-file-read-shell-result.sh: add Q/A

--- t-file-read-shell-result.sh
+++ t-file-read-shell-result.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 #
-# real    0m0.428s val=$(shell}
+# Q: Which one is faster: just $() or saving content to file and then reading file?
+# A: The $() is faster than using a temporary file
+#
+# real    0m0.428s val=$(shell)
 # real    0m0.899s shell > file; val=$(< file)
 
 . ./t-lib.sh ; f=$rand
