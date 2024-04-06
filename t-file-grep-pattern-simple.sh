2792dc62ae31c934de122f17fd024f5feadce71b
Jari Aalto <jari.aalto@cante.net>
2024-04-06 19:49:06 +0300

    t-file-grep-pattern-simple.sh: new

---
+++ t-file-grep-pattern-simple.sh
@@ -0,0 +1,86 @@
+#! /bin/bash
+#
+# Q: to match file, how much more efficient is read to grep?
+# A: Yes, much faster (because grep launces separate process)
+#
+# NOTE: this is only for brief checks if FILE contain PATTERN. Not a substitute
+# for real regular expressions.
+#
+# t1 real   0m0.183s read + case
+# t2 real   0m0.184s read + bash regexp
+# t3 real   0m0.396s grep
+
+. ./t-lib.sh ; f=$rand
+
+tmp=t.tmp
+string=abc
+
+{ echo "$string $string" ; cat $f; } > $tmp
+
+f=$tmp
+
+MathFileContentPattern ()  # POSIX
+{
+    local file=$1
+    local pattern=$2
+
+    read -N10000 < "$file"
+
+    case "$REPLY" in
+        $pattern)
+            return 0
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+MathFileContentRegexp () # BASH REGEXP
+{
+    local file=$1
+    local re=$2
+
+    read -N10000 < "$file"
+
+    [[ "$REPLY" =~ $re ]]
+}
+
+t1 ()
+{
+    for i in {1..100}
+    do
+        MathFileContentPattern $f "$string*$string"
+    done
+}
+
+t2 ()
+{
+    for i in {1..100}
+    do
+        MathFileContentRegexp $f "$string*$string"
+    done
+}
+
+t3 ()
+{
+    for i in {1..100}
+    do
+        grep --quiet --files-with-matches "$string.*$string" $f
+    done
+}
+
+t ()
+{
+    echo -n "# $1"
+    time $1
+    echo
+}
+
+t t1
+t t2
+t t3
+
+rm -f $tmp
+
+# End of file
