e2c4fe52bb64c17b26651281965c4cfc67a0e32b
Jari Aalto <jari.aalto@cante.net>
2024-04-06 20:02:17 +0300

    t-lib.sh: add comment

--- t-lib.sh
+++ t-lib.sh
@@ -1,11 +1,16 @@
-#! /bin/bash
-# source <file>
+#  -*- mode: sh; -*-
+#
+# This is not a script but a library.
+#
+# Synopsis: source <file>
 
-rand=t.random.numbers.tmp
+rand=t.random.numbers.tmp  # create test file
 
 if [ ! -f $f ]; then
     n=10000   # 10 000 numbers
-    perl -e "print int(rand(2**14-1)) . qq(\n) for 1..$n" > $rand
+    perl -e "print int(rand(2**14-1)) . qq(\n) for 1..$n" > "$rand"
 fi
+
+unset n
 
 # End of file
