7d48081acf86b3302f12c3babf5f1f736a4f5e48
Jari Aalto <jari.aalto@cante.net>
2025-02-10 19:35:38 +0200

    t-array-split-string.bash: update

--- t-array-split-string.bash
+++ t-array-split-string.bash
@@ -1,15 +1,22 @@
 #! /bin/bash
 #
+# Q: split string into array: read vs eval?
+# A: eval is 5x faster
+#
 # real    0m0.025s read -ra
 # real    0m0.005s eval     (!)
+
+. ./t-lib.sh ; f=$random_file
 
 declare -a array
 string=$(echo {1..100})
 
 t1 ()
 {
-    for i in {1..100}
+    i=1
+    while [ $i -le $loop_max ]
     do
+        i=$((i + 1))
         # Slow, because internally uses temporary file to store STRING.
         IFS=', ' read -ra array <<< "$string"
         i=${array[0]}
@@ -18,19 +25,13 @@ t1 ()
 
 t2 ()
 {
-    for i in {1..100}
+    i=1
+    while [ $i -le $loop_max ]
     do
-        # IFS is temporary with 'eval'
+        i=$((i + 1))
         IFS=', ' eval 'array=($string)'
         i=${array[0]}
     done
-}
-
-t ()
-{
-    echo -n "# $1"
-    time $1
-    echo
 }
 
 t t1
