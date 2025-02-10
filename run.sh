e7ede747f70b4daa7b6559992969ad2372036da2
Jari Aalto <jari.aalto@cante.net>
2025-02-10 19:02:29 +0200

    run.sh: new

---
+++ run.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+
+PROGRAM=${0##*/}
+LINE=$(printf '%*s' "55" '' | tr ' ' '-')
+
+FileInfo ()
+{
+    awk '
+        /^#!/, /^$/ {
+            print
+        }
+
+    ' "$1"
+}
+
+Header ()
+{
+    echo "-- $LINE"
+    echo "-- $1"
+    echo "-- $LINE"
+}
+
+Run ()
+{
+    Header "$1"
+    FileInfo "$1"
+    "./$1"
+}
+
+Main ()
+{
+    if [ ! "$1" ]; then
+        echo "Synopsis: $PRORAM <test file to run> ..."
+        return 1
+    fi
+
+    for file in "$@"
+    do
+        Run "$file"
+    done
+}
+
+Main "$@"
+
+# End of file
