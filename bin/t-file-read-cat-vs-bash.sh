8af3b5eac865cf90ed1686b98651659b5c113ec8
Jari Aalto <jari.aalto@cante.net>
2025-02-11 00:47:38 +0200

    t-file-read-cat-vs-bash.sh: update

--- bin/t-file-read-cat-vs-bash.sh
+++ bin/t-file-read-cat-vs-bash.sh
@@ -1,18 +1,23 @@
 #! /bin/bash
 #
-# Q: How much is $(< FILE) faster than $(cat FILE)?
-# A: bash $(< FILE) is about 2x faster
+# Q: Is Bash $(< FILE) faster than $(cat FILE)?
+# A: The $(< FILE) is about 2x faster for small files
 #
 # real    0m0.166s $(< file)
 # real    0m0.365s $(cat file)
 #
-# NOTES: out of interest, cat is faster with big files:
+# Notes:
 #
-# time bash -c 'cat FILE_1M > /dev/null'
+# With big files, cat(1) is much faster:
+#
+# . t-lib.sh
+# RandomWordsDictionary 1M > t.1M
+#
+# time bash -c 'cat t.1M > /dev/null'
 # real    0m0.014s
 #
-# time bash -c 's=$(< FILE_1M); echo "$s" > /dev/null'
-# real  0m0.086s
+# time bash -c 's=$(< t.1M); echo "$s" > /dev/null'
+# real  0m0.055s
 
 . ./t-lib.sh ; f=$random_file
 
