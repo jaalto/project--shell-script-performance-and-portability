f1c972758a27b338a578d469127589d82c662acb
Jari Aalto <jari.aalto@cante.net>
2025-02-10 20:09:58 +0200

    t-file-copy-check-exist.sh: update

--- bin/t-file-copy-check-exist.sh
+++ bin/t-file-copy-check-exist.sh
@@ -1,17 +1,22 @@
 #! /bin/bash
 #
-# Q: Need a COPY of file. Call cp(1), hardlink, or test -nt before copy?
-# A: test -nt before cp(1) is fastestm hardlink is fast too.
+# Q: Need a copy of file. Call cp(1), make hardlink, or do test before copy?
+# A: Faster is to test existense of file before cp(1). Hardlink is fast.
 #
-# real    0m0.103s $(< file)
-# real    0m0.002s [ -s file] && $(< file)
+# t1 real    0m1.002s cp A B
+# t2 real    0m0.013s test(1) -nt ...
+# t2 real    0m0.009s test(1) -ef ... (using hardlink)
+#
+# Code:
+#
+# cp --preserve=timestamps A B                       # t1
+# [ A -nt B ] || cp --preserve=timestamps ...        # t2
+# [ A -ef B ] || cp --preserve=timestamps --link ... # t3
 
 . ./t-lib.sh ; f=$random_file
 
 copy="$f.copy.tmp"
 rm --force "$copy"
-
-set -x
 
 AtExit ()
 {
@@ -31,6 +36,20 @@ t1 ()
 
 t2 ()
 {
+    # when called: $copy already exist (as it should)
+
+    i=1
+    while [ $i -le $loop_max ]
+    do
+        i=$((i + 1))
+        if [ $f -nt $copy ]; then # if newer file, then copy
+            cp --preserve=timestamps $f $copy
+        fi
+    done
+}
+
+t3 ()
+{
     i=1
     while [ $i -le $loop_max ]
     do
@@ -41,24 +60,12 @@ t2 ()
     done
 }
 
-t3 ()
-{
-    # when called: $copy already exist (as it should)
-
-    i=1
-    while [ $i -le $loop_max ]
-    do
-        i=$((i + 1))
-        if [ $copy -nt $f ]; then
-            cp --preserve=timestamps $f $copy
-        fi
-    done
-}
-
-trap AtExit EXIT HUP INT QUIT TERM
+#trap AtExit EXIT HUP INT QUIT TERM
 
 t t1
+rm --force "$copy"
 t t2
+rm --force "$copy"
 t t3
 
 # End of file
