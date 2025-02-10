9e93ce03e2133a0ab160e0977df34b14afa9cbd7
Jari Aalto <jari.aalto@cante.net>
2025-02-10 21:49:11 +0200

    t-file-newest-in-dir.sh: update

--- bin/t-file-newest-in-dir.sh
+++ bin/t-file-newest-in-dir.sh
@@ -1,52 +1,72 @@
 #! /bin/bash
 #
 # Q: What is the fastest way to get newest file in directory
-# A: find + awk is almost as fast as "ls -t" (but that would compare both dirs and files)
+# A: find + awk is tad faster but more complex. Use find + filters.
 #
-# t1 real    0m0.054s   find + awk
-# t2 real    0m0.184s   find + sort + head + cut
-# t3 real    0m0.200s   find + sort + sed
-# t4 real    0m0.039s   stat
-# t5 real    0m0.032s   ls -t
+# t1 real    0m0.417s   find + awk
+# t2 real    0m0.523s   find + sort + head + cut
+# t3 real    0m0.575s   find + sort + sed
 #
-# NOTES
+# t4 real    0m0.382s   stat (not a generic solution; see awk)
+# t5 real    0m0.330s   ls -t (not a generic solution; see awk)
+#
+# Code:
+#
+# See <file>.sh for more details.
+#
+# find -maxdepth 1 -type f ... | awk '<complex code>'       # t1
+# find -maxdepth 1 -type f | sort -r | head -1 | cut ...    # t2
+# find -maxdepth 1 -type f | sort -r | sed ...              # t3
+# stat ... | sort -r | sed ...                              # t4
+# ls --sort=time | head -1                                  # t5
+#
+#
+# Notes:
 #
 # awk(1) binary is smaller that sed(1)
 #
 # Probably small head(1) and cut(1) combined is still
-# faster than sed(1) with regexp engine.
+# faster than sed(1) which uses regexp engine.
 #
 # These can't tell files from directories:
 #
 #   ls -t   sort by time
 #   stat
 
-TMPBASE=tmp.$$
-DIR=t
+. ./t-lib.sh ; f=$random_file
+
+pwd=$(cd "$(dirname "$0")" && pwd)
+max_files=${max_files:-100}
+DIR=$(mktemp --directory --tmpdir="$pwd")
 
 AtExit ()
 {
-    [ "$TMPBASE" ] || return 0
+    [ -d "$DIR" ] || return 0
 
-    rm -f $DIR/"$TMPBASE"*
+    rm --recursive --force "$DIR/"
 }
 
-prep ()
+Setup ()
 {
-    mkdir -p $DIR
+    mkdir --parents "$DIR"
 
     # Generate files 1h apart
 
-    for i in {1..100}
+    i=1
+    while [ $i -le $max_files ]
     do
-        touch --date="-$i hours" $DIR/$TMPBASE.$i || exit $?
+        i=$((i + 1))
+        touch --date="-$i hours" "$DIR/$i" || exit $?
     done
 }
 
 t1 ()
 {
-    for i in {1..10}
+    i=1
+    while [ $i -le $loop_max ]
     do
+        i=$((i + 1))
+
         find . -maxdepth 1 -type f -printf "%T@ %p\n" |
         awk '
             {
@@ -63,10 +83,14 @@ t1 ()
 
 t2 ()
 {
-    for i in {1..10}
+    i=1
+    while [ $i -le $loop_max ]
     do
+        i=$((i + 1))
         find $DIR -maxdepth 1 -type f -printf "%T@ %p\n" |
-            sort -r | head -1 | cut -d' ' -f2 \
+            sort --reverse |
+            head --lines=1 |
+            cut --delimiter=' ' --fields=2 \
             > /dev/null
     done
 }
@@ -74,52 +98,46 @@ t2 ()
 
 t3 ()
 {
-    for i in {1..10}
+    i=1
+    while [ $i -le $loop_max ]
     do
+        i=$((i + 1))
         find $DIR -maxdepth 1 -type f -printf "%T@ %p\n" |
-            sort -r | sed -En 's,^.+ ,,; 1p; q' \
+            sort --reverse |
+            sed --regexp-extended --quiet 's,^.+ ,,; 1p; q' \
             > /dev/null
     done
 }
 
 t4 ()
 {
-    for i in {1..10}
+    i=1
+    while [ $i -le $loop_max ]
     do
+        i=$((i + 1))
         stat --format="%Y %n" * |
-            sort -r | sed -En 's,^.+ ,,; 1p; q' \
+            sort --reverse |
+            sed --regexp-extended --quiet 's,^.+ ,,; 1p; q' \
             > /dev/null
     done
 }
 
 t5 ()
 {
-    for i in {1..10}
+    i=1
+    while [ $i -le $loop_max ]
     do
-        ls -t | head --lines=1 \
-            > /dev/null
+        i=$((i + 1))
+        ls --sort=time | head --lines=1 > /dev/null
     done
 }
 
-t ()
-{
-    echo -n "# $1"
-    time $1
-    echo
-}
-
 trap AtExit EXIT HUP INT QUIT TERM
-prep
+Setup
 
 t t1
-
-# Load Kernel cache by running pipes multiple times: t2, t3
 t t2
-t t2
-
 t t3
-t t3
-
 t t4
 t t5
 
