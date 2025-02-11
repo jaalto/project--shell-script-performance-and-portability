8b277202ab1537e9525396a416b3424b7446091c
Jari Aalto <jari.aalto@cante.net>
2025-02-11 20:31:56 +0200

    t-array-split-string.sh: commentary

--- bin/t-array-split-string.sh
+++ bin/t-array-split-string.sh
@@ -1,16 +1,22 @@
 #! /bin/bash
 #
-# Q: split string into array: read vs eval?
-# A: eval is 5x faster
+# Q: split string into an array: read vs eval?
+# A: eval is 2x faster
 #
-# t1 real    0m0.025s read -ra
-# t2 real    0m0.005s eval     (!)
+# t1 real    0m0.012s eval
+# t2 real    0m0.025s read -ra
 #
 # Code:
 #
 # string=$(echo {1..100})
 # read -ra array <<< "$string"  # t1
 # eval 'array=($string)'        # t2
+#
+# Notes:
+#
+# The reason is probably that `<<<` uses a
+# temporary file, whereas `eval` operates
+# entirely in memory.
 
 . ./t-lib.sh ; f=$random_file
 
@@ -21,8 +27,7 @@ t1 ()
 {
     for i in $(seq $loop_max)
     do
-        # Slow, because internally uses temporary file to store STRING.
-        IFS=', ' read -ra array <<< "$string"
+        IFS=', ' eval 'array=($string)'
         item=${array[0]}
     done
 }
@@ -31,7 +36,8 @@ t2 ()
 {
     for i in $(seq $loop_max)
     do
-        IFS=', ' eval 'array=($string)'
+        # Slow, because internally uses temporary file to store STRING.
+        IFS=', ' read -ra array <<< "$string"
         item=${array[0]}
     done
 }
