d9c0c73bbee779d040bb28131e40e2dfbdbb58e6
Jari Aalto <jari.aalto@cante.net>
2025-02-11 03:12:33 +0200

    statement-increment.sh: update

--- bin/statement-increment.sh
+++ bin/statement-increment.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 #
-# Q: POSIX ': $((i++))' vs ((i++)) vs let i++?
+# Q: POSIX 'i=$((i + 1))' vs ((i++)) vs let i++ etc.
 # A: No noticeable difference
 #
 # t1     real    0m0.005s ((i++))
 # t2     real    0m0.005s let i++
 # t3     real    0m0.007s : $((i++))
+# t4     real    0m0.007s i=$((i + 1))
 
 . ./t-lib.sh ; f=$random_file
+
+loop_max=${max_count:-1000}
 
 t1 ()
 {
@@ -37,8 +40,18 @@ t3 ()
     done
 }
 
+t4 ()
+{
+    item=0
+    for i in $(seq $loop_max)
+    do
+        item=$((item + 1))  # traditional
+    done
+}
+
 t t1
 t t2
 t t3
+t t4
 
 # End of file
