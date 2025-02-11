1849dfc7ba00c7c2f823f9ad6a8c5eddd78892b5
Jari Aalto <jari.aalto@cante.net>
2025-02-11 02:56:14 +0200

    statement-if-posix-vs-bash.sh: new

---
+++ bin/statement-if-posix-vs-bash.sh
@@ -0,0 +1,80 @@
+#! /bin/bash
+#
+# Q: POSIX [ $var = 1 ] vs Bash [[ $var = 1 ]] etc
+# A: In practise, not much difference
+#
+# t1val     real    0m0.002s [ "$var" = "1" ]
+# t2val     real    0m0.003s [[ $var = 1 ]]
+# t1empty   real    0m0.002s [ ! "$var" ]     # modern
+# t2empty   real    0m0.002s [ -z "$var" ]    # archaic
+# t3empty   real    0m0.003s [[ ! $var ]]     # Bash
+#
+# Notes:
+#
+# With very high amount of repeats, there are differences.
+# Like if ooping files with lot of lines and making
+# decisions.
+#
+# loop_max=10000 ./statement-if-posix-vs-bash.sh
+#
+# t1val          real 0.055  user 0.054  sys 0.000
+# t2val          real 0.032  user 0.030  sys 0.003  [[ ]]
+# t1empty        real 0.052  user 0.045  sys 0.007
+# t2empty        real 0.053  user 0.050  sys 0.003
+# t3empty        real 0.032  user 0.026  sys 0.007  [[ ]]
+
+. ./t-lib.sh ; f=$random_file
+
+t1val ()
+{
+    var="1"
+    for i in $(seq $loop_max)
+    do
+        [ "$var" = "1" ]
+    done
+}
+
+t2val ()
+{
+    var="1"
+    for i in $(seq $loop_max)
+    do
+        [[ $var = 1 ]]
+    done
+}
+
+t1empty ()
+{
+    var="abc"
+    for i in $(seq $loop_max)
+    do
+        [ ! "$var" ]
+    done
+}
+
+t2empty ()
+{
+    var="abc"
+    for i in $(seq $loop_max)
+    do
+        [ -z "$var" ]
+    done
+}
+
+t3empty ()
+{
+    var="abc"
+    for i in $(seq $loop_max)
+    do
+        [[ ! $var ]]
+    done
+}
+
+
+t t1val
+t t2val
+t t1empty
+t t2empty
+t t3empty
+
+# End of file
