f27943f203ba1df5cd2c9ba486761da262e3af95
Jari Aalto <jari.aalto@cante.net>
2025-02-12 07:26:16 +0200

    t-function-and-return-value.sh: update

--- bin/t-function-and-return-value.sh
+++ bin/t-function-and-return-value.sh
@@ -1,47 +1,49 @@
 #! /bin/bash
 #
 # Q: Bash name ref to return values vs val=$(funcall)
-# A: name ref is about 40x faster
+# A: Using name ref is about 40x faster
 #
 # t1 real    0m0.089s t1 $(funcall)
 # t2 real    0m0.002s t2 funcall nameref
 #
 # Code:
 #
-# fn(): return "echo <value>"     # t1
-# fn(): return using local -n var # t2
+# fn(): ... echo "<value>"                 # t1
+# fn(): local -n ret=$1; ... ret="<value>" # t2
 
 . ./t-lib.sh ; f=$random_file
 
 f1 ()
 {
-    a="done"
-    echo $a
+    result="return value"
+    echo "$result"
 }
 
 f2 ()
 {
-    local -n var=$1  # nameref attribute, reference to variable
-    var="done"
+    local -n retval=$1  # nameref attribute, a reference to variable
+
+    result="return value"
+    retval="$result"
 }
 
 t1 ()
 {
-    local v=0
+    val=""
 
     for i in {1..100}
     do
-        v=$(f1)
+        val=$(f1)
     done
 }
 
 t2 ()
 {
-    local v=0
+    val=""
 
     for i in {1..100}
     do
-        f2 v
+        f2 val
     done
 }
 
