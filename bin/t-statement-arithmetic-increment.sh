bfe037c8397a1109775310b0e83654d041d79df0
Jari Aalto <jari.aalto@cante.net>
2025-02-12 07:38:44 +0200

    t-statement-arithmetic-increment.sh: update

--- bin/t-statement-arithmetic-increment.sh
+++ bin/t-statement-arithmetic-increment.sh
@@ -1,18 +1,26 @@
 #! /bin/bash
 #
 # Q: POSIX `i=$((i + 1))` vs `((i++))` vs `let i++` etc.
-# A: No noticeable difference
+# A: No noticeable difference, POSIX ´i=$((i + 1))` will do fine
 #
-# t1     real    0m0.005s ((i++))      Bash
-# t2     real    0m0.005s let i++      Bash
-# t3     real    0m0.007s : $((i++))   POSIX
-# t4     real    0m0.007s i=$((i + 1)) POSIX
+# t1     real    0m0.025s ((i++))      Bash
+# t2     real    0m0.047s let i++      Bash
+# t3     real    0m0.045s i=$((i + 1)) POSIX
+# t4     real    0m0.061s : $((i++))   POSIX (side effect)
+#
+# Notes:
+#
+# The tests were using 10,000 repeats, which is
+# unrealistic for any program. There really is no
+# practical difference whichever you choose. The
+# portable POSIX version works in all shells:
+# `i=$((i + 1))`.
 
 [ "${loop_max:+user}" = "user" ] && loop_count=$loop_max
 
 . ./t-lib.sh ; f=$random_file
 
-loop_max=${loop_count:-1000}
+loop_max=${loop_count:-10000}
 
 t1 ()
 {
@@ -38,7 +46,7 @@ t3 ()
     item=0
     for i in $(seq $loop_max)
     do
-        : $((item++))
+        item=$((item + 1))  # traditional
     done
 }
 
@@ -47,7 +55,7 @@ t4 ()
     item=0
     for i in $(seq $loop_max)
     do
-        item=$((item + 1))  # traditional
+        : $((item++))
     done
 }
 
