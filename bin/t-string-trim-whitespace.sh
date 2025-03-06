8874949998f739d89f1001e64b099a418bc05a20
Jari Aalto <jari.aalto@cante.net>
2025-03-06 15:54:42 +0200

    bin/t-string-trim-whitespace.sh: update

--- bin/t-string-trim-whitespace.sh
+++ bin/t-string-trim-whitespace.sh
@@ -3,15 +3,15 @@
 # Q: Trim whitepace using Bash RE vs sed(1)
 # A: Bash is much faster; especially with fn() using nameref
 #
-# t2 real    0m0.025s Bash fn() RE, using nameref for return value
-# t2 real    0m0.107s Bash fn() RE
-# t1 real    0m0.440s echo | sed RE
+#     t2 real    0m0.025s Bash fn() RE, using nameref for return value
+#     t2 real    0m0.107s Bash fn() RE
+#     t1 real    0m0.440s echo | sed RE
 #
 # Code:
 #
-# t1 BashTrim var                    # fn() using nameref
-# t2 var=$(bashTrim "$str")          # fn() return by value
-# t3 var=$(echo "$str" | sed <trim>) # subshell call
+#     t1 BashTrim var                    # fn() using nameref
+#     t2 var=$(bashTrim "$str")          # fn() return by value
+#     t3 var=$(echo "$str" | sed <trim>) # subshell call
 
 . ./t-lib.sh ; f=$random_file
 
