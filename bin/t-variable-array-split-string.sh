7a3101143651b2a1169464a03dd057e813c4881c
Jari Aalto <jari.aalto@cante.net>
2025-03-06 16:24:08 +0200

    bin/t-variable-array-split-string.sh: update

--- bin/t-variable-array-split-string.sh
+++ bin/t-variable-array-split-string.sh
@@ -15,7 +15,7 @@
 #
 # This test involves splitting by an arbitrary
 # character, which requires setting a local
-# IFS for the execution of the command.
+# `IFS` for the execution of the command.
 #
 # The reason why `<<<` is slower is that it
 # uses a pipe buffer (in latest Bash),
