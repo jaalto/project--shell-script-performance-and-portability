42bd8c59417ab594a34b2d9f3b62c300d36e8ec9
Jari Aalto <jari.aalto@cante.net>
2025-03-06 16:36:13 +0200

    bin/t-file-read-cat-vs-bash.sh: update

--- bin/t-file-read-cat-vs-bash.sh
+++ bin/t-file-read-cat-vs-bash.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #
 # Q: Howabout `$(< FILE)` vs `$(cat FILE)`
-# A: The `$(< FILE)´ is about 2x faster for small files
+# A: The `$(< FILE)` is about 2x faster for small files
 #
 #     t1 real 0m0.166s $(< file)
 #     t2 real 0m0.365s $(cat file)
