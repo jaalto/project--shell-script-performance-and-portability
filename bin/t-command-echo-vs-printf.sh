b38146715b6c9ff99b4bddd011f7a429493a0dc9
Jari Aalto <jari.aalto@cante.net>
2025-03-07 11:48:32 +0200

    ./t-command-echo-vs-printf.sh: new

---
+++ bin/t-command-echo-vs-printf.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+#
+# Q: The classic: `echo` vs `printf'
+# A: Rhere is no real difference
+# priority: 0
+#
+#     t1 real 0m0.272s echo
+#     t2 real 0m0.278s printf
+
+. ./t-lib.sh ; f=$random_file
+
+t1 ()
+{
+    while read -r line
+    do
+        echo "$line" > /dev/null
+    done < $f
+}
+
+t2 ()
+{
+    while read -r line
+    do
+        printf "%s\n" "$line" > /dev/null
+    done < $f
+}
+
+t t1
+t t2
+
+# End of file
