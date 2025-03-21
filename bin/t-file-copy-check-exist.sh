7ae9fcefca006a6f1fe6bfad7f702a0560a0a905
Jari Aalto <jari.aalto@cante.net>
2025-03-21 15:45:05 +0200

    t-file-copy-check-exist.sh: use RunTests framework

--- bin/t-file-copy-check-exist.sh
+++ bin/t-file-copy-check-exist.sh
@@ -1,33 +1,26 @@
 #! /bin/bash
 #
 # Q: Should you test existense before copying?
-# A: It is about 50x faster is you test existense before copying.
+# A: It is about 40x faster is you test existense before copying.
 # priority: 1
 #
-#     t1 real 0m0.013s <file test> cp
-#     t2 real 0m0.009s <file test> cp (hardlink)
-#     t3 real 0m1.812s cp A B
+#     t1 real 0m0.007s <file test> cp
+#     t2 real 0m1.270s cp A B
 #
 # Code:
 #
 #     t1 [ A -nt B ] || cp --preserve=timestamps ...
-#     t2 [ A -ef B ] || cp --preserve=timestamps --link ...
-#     t3 cp --preserve=timestamps A B
+#     t2 cp --preserve=timestamps A B
 
 . ./t-lib.sh ; f=$random_file
 
-copy="$f.copy.tmp"
-rm --force "$copy"
-
-AtExit ()
-{
-    [ "$copy" ] || return 0
-    rm --force "$copy"
-}
+f=$TMPBASE.file
+copy=$f.copy
+: > $f
 
 t1 ()
 {
-    # when called: $copy already exist (as it should)
+    # When called: $copy must already exist
 
     for i in $(seq $loop_max)
     do
@@ -41,33 +34,17 @@ t2 ()
 {
     for i in $(seq $loop_max)
     do
-        if [ ! -e $copy ]; then
-            if [ ! $f -ef $copy ]; then  # not the same hardlink?
-                cp --preserve=timestamps --link $f $copy # make a hardlink
-            fi
-        fi
-    done
-}
-
-t3 ()
-{
-    for i in $(seq $loop_max)
-    do
         cp --preserve=timestamps $f $copy
     done
 }
 
+EnableDefaultTrap
 
-trap AtExit EXIT HUP INT QUIT TERM
+t="\
+:t t1
+:t t2
+"
 
-rm --force "$copy"
-t t1
-
-rm --force "$copy"
-t t2
-
-rm --force "$copy"
-t t3
-
+RunTests "$t" "$@"
 
 # End of file
