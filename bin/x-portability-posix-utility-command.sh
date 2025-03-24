09d329dcde90017c546621284eecfa394d8ee49e
Jari Aalto <jari.aalto@cante.net>
2025-03-24 16:45:45 +0200

    x-portability-posix-utility-command.sh: (doc): update

--- bin/x-portability-posix-utility-command.sh
+++ bin/x-portability-posix-utility-command.sh
@@ -1,24 +1,4 @@
 #! /bin/sh
-# Q: POSIX support: command -v, type, hash
+# Q: Test POSIX support: command -v
 
-Run ()
-{
-    tmp=t.tmp
-
-    "$@" > $tmp 2>&1
-
-    status=$?
-    output=""
-
-    [ -s $tmp ] && output=$(cat $tmp | tr -d '\n')
-
-    [ "$output" ] && output=" $output"
-
-    printf "# status: %-3d cmd: %s output:%s\n" $status "$*" "$output"
-
-    rm $tmp
-}
-
-Run command -v ls
-Run type ls
-Run hash ls
+command -v ls
