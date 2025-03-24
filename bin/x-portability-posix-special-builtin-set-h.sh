e7b92bf65c132c3aa46ab4ede44e8960ea785a81
Jari Aalto <jari.aalto@cante.net>
2025-03-24 21:53:44 +0200

    x-portability-posix-special-builtin-set-h.sh: new

---
+++ bin/x-portability-posix-special-builtin-set-h.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+# Short: POSIX set -h
+# Desc: Test POSIX support: set -h (Locate and remember utilities)
+# Url: https://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#tag_18_25_01
+
+set -h
+command ls
+
