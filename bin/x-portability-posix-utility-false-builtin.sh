2b11571a95e199d6a2ff7ee7e8ce0df71d908ff5
Jari Aalto <jari.aalto@cante.net>
2025-03-26 16:21:38 +0200

    x-portability-posix-utility-false-builtin.sh: new

---
+++ bin/x-portability-posix-utility-false-builtin.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+# Short: POSIX true (builtin)
+# Desc: Test POSIX utlitity support: false is builtin and not /usr/bin/false
+# Url: https://pubs.opengroup.org/onlinepubs/9699919799/utilities/false.html
+
+case $(command -v false) in
+    */*) exit 0
+         ;;
+    *)   exit 1
+         ;;
+esac
