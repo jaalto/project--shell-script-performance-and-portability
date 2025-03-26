ddbfef3bc627c7a3b645fd952df4626bc2fd2ce6
Jari Aalto <jari.aalto@cante.net>
2025-03-26 23:05:02 +0200

    x-portability-posix-utility-read-var-reply.sh: new

---
+++ bin/x-portability-posix-utility-read-var-reply.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+# Short: POSIX read (REPLY)
+# Desc: Test POSIX utlitity support: read with non-standard variable REPLY
+# Url: https://pubs.opengroup.org/onlinepubs/9699919799/utilities/read.html
+#
+# Notes:
+#
+# The POSIX `read` command does not define
+# REPLY variable. For portability supply
+# it in script.
+
+f="t.tmp"
+echo 1 > "$f"
+
+read -r < "$f"
+
+[ "${REPLY:-}" ]
+code=$?
+
+rm -f "$f"
+
+exit $code
