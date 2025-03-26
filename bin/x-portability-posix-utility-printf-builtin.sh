eb4187ab9a3c09fb24349974f6597a4109ae404a
Jari Aalto <jari.aalto@cante.net>
2025-03-26 16:22:09 +0200

    x-portability-posix-utility-printf-builtin.sh: (Url): add

--- bin/x-portability-posix-utility-printf-builtin.sh
+++ bin/x-portability-posix-utility-printf-builtin.sh
@@ -1,15 +1,11 @@
 #! /bin/sh
 # Short: POSIX printf (builtin)
 # Desc: Test POSIX utlitity support: printf is builtin and not /usr/bin/printf
+# Url: https://pubs.opengroup.org/onlinepubs/9699919799/utilities/printf.html
 
-test ()
-{
-    case $(command -v printf) in
-        */*) return 1
-        ;;
-    esac
-
-    return 0
-}
-
-test
+case $(command -v printf) in
+    */*) exit 0
+         ;;
+    *)   exit 1
+         ;;
+esac
