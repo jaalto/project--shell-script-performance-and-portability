183f6dba5040fcc09132081bf4290be02a4fe140
Jari Aalto <jari.aalto@cante.net>
2025-03-29 12:42:22 +0200

    x-portability-posix-special-builtin-export-f.sh: new

---
+++ bin/x-portability-posix-special-builtin-export-f.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+# Short: POSIX export (-f)
+# Desc: Test POSIX Special Built-in support: export with non-standard option -f
+# Url: https://pubs.opengroup.org/onlinepubs/9799919799/utilities/V3_chap02.html#export
+
+fn ()
+{
+    :
+}
+
+export -f fn
+
+( fn )
+
+exit $?
