57a8e98b26622635bba9913a9d7bbed17d74e35f
Jari Aalto <jari.aalto@cante.net>
2025-11-13 17:13:18 +0200

    x-portability-here-string.sh: (Notes): add

--- bin/x-portability-here-string.sh
+++ bin/x-portability-here-string.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 # Short: <<< HERE_STRING
 # Desc: Test for HERE STRING <<< support
+#
+# Notes:
+#
+# Only HERE DOCUMENT syntax '<<' and '<<-' is defined in POSIX
+# Url: https://pubs.opengroup.org/onlinepubs/9799919799.2024edition/utilities/V3_chap02.html#tag_19_07_04
 
 : <<< "string"
