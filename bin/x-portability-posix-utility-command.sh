9384b7f5cf3ecee6dee811dd7818f57791fc2d7a
Jari Aalto <jari.aalto@cante.net>
2025-11-13 17:13:29 +0200

    x-portability-posix-utility-command.sh: (Url): add

--- bin/x-portability-posix-utility-command.sh
+++ bin/x-portability-posix-utility-command.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 # Short: POSIX command -v
 # Desc: Test POSIX utlitity support: command -v
+# Url: https://pubs.opengroup.org/onlinepubs/9799919799/utilities/command.html
 
 command -v ls
