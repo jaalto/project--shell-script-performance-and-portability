35e280955079d97287aae725742eabc3551b0f9d
Jari Aalto <jari.aalto@cante.net>
2025-11-13 17:15:06 +0200

    x-portability-posix-utility-timeout.sh: new

---
+++ bin/x-portability-posix-utility-timeout.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+# Short: POSIX timeout
+# Desc: Test POSIX utlitity support: timeout
+# Url: https://pubs.opengroup.org/onlinepubs/9799919799/utilities/timeout.html
+#
+# Notes:
+#
+# Added in POSIX.1-2004
+
+type timeout
