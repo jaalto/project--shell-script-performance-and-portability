91c21067950adba915cd23ea45528b4a6837ece8
Jari Aalto <jari.aalto@cante.net>
2025-12-17 23:41:50 +0200

    run-all.sh: add parallel support

--- bin/run-all.sh
+++ bin/run-all.sh
@@ -1,8 +1,21 @@
 #! /bin/sh
 # Run all test cases
 
+LIB="t-lib.sh"
+
+. "./$LIB"
+
 shells=${1:-dash,ksh93,bash}
 
-{ ./run.sh --shell "$shells" $(ls ./t-*.sh | grep -v lib) ; } 2>&1
+{
+    if IsCommandParallel; then
+        ${test:+echo} "$PARALLEL" --keep-order --quote \
+            ./run.sh --shell "$shells" ::: \
+            $(ls ./t-*.sh | grep -v "$LIB")
+    else
+        ${test:+echo} ./run.sh --shell "$shells" $(ls ./t-*.sh | grep -v "$LIB")
+    fi
+
+} 2>&1
 
 # End of file
