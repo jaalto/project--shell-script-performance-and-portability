121faeefd223db53bd081d9373b1e001ffc3eb40
Jari Aalto <jari.aalto@cante.net>
2025-12-18 20:43:11 +0200

    run-all.sh: rewite to proper shell script

--- bin/run-all.sh
+++ bin/run-all.sh
@@ -19,40 +19,128 @@
 #       You should have received a copy of the GNU General Public License
 #       along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-# Description
-#
-#       Run all test cases.
-#
 # Usage
 #
-#       ./run-all.sh [<comma separated list of shells>]
-#
-# Examples
-#
-#       ./run-all.sh dash,ksh93,bash
-#       ./run-all.sh > results.txt  # same as above
+#       See the --help option.
 
 set -o errexit   # Exit on error
 set -o nounset   # Treat unused variables as errors
 
+SHELL_DEFAULT="dash,ksh,bash"
+
+PARALLEL="parallel"
+PROGRAM="$0"
 LIB="t-lib.sh"
 
-. "./$LIB"
+# ignore follow
+# shellcheck disable=SC1090
 
-shells=${1:-dash,ksh93,bash}
+source="source-only" . "$LIB"
 
+Help ()
 {
-    # Disable. Parallel not used.
-    # The 'time' results are not displayed properly
+    program=$PROGRAM
 
-    if [ "" ] && IsCommandParallel; then
-        ${test:+echo} "$PARALLEL" --keep-order --quote \
-            ./run.sh --shell "$shells" ::: \
-            $(ls ./t-*.sh | grep -v "$LIB")
-    else
-        ${test:+echo} ./run.sh --shell "$shells" $(ls ./t-*.sh | grep -v "$LIB")
+    case $program in
+        */*) ;;
+          *) program="./$program"
+             ;;
+    esac
+
+    echo "\
+SYNOPSIS
+    $program [[--shell] SHELL-LIST}
+
+OPTIONS
+    -s, --shell SHELL-LIST
+        Run <test case> file using SHELL-LIST.
+        The list is separated by commas. This could
+        include 'dash', 'posh', 'ksh', 'mksh',
+        'busybox ash' etc.
+        Default is: SHELL_DEFAULT
+
+    -h, --help
+        Display help.
+
+DESCRIPTION
+    Run all test cases under --shell SHELL-LIST
+
+EXAMPLES
+
+    $program # use defaults
+    $program dash,ksh93,bash"
+
+    exit 0
+}
+
+Main ()
+{
+    local shlist
+    shlist="$SHELL_DEFAULT"
+
+    local parallel
+    parallel=""
+
+
+    local dummy
+
+    while :
+    do
+        # Unused, but useful during debug
+        # shellcheck disable=SC2034
+        dummy="OPT: ${1:-}"
+
+        case ${1:-} in
+            -s | --shell)
+                shift
+                [ "${1:-}" ] || Die "ERROR: missing --shell ARG"
+                shlist=$1
+                shift
+                ;;
+            -h | --help)
+                shift
+                Help
+                ;;
+            --)
+                shift
+                break
+                ;;
+            -*)
+                Warn "$PROGRAM: WARN Unknown option: $1"
+                shift
+                ;;
+            *)
+                break
+                ;;
+        esac
+    done
+
+    if [ "${1:-}" ]; then
+        shlist="$1"
     fi
 
-} 2>&1
+    if IsCommandParallel; then
+        # Not activated.
+        # The 'time' results under parallel are not
+        # displayed properly
+        :
+    fi
+
+    if [ "$parallel" ]; then
+        # ignore quotes
+        # shellcheck disable=SC2010,SC2046
+
+        ${test:+echo} "$PARALLEL" --keep-order --quote \
+            ./run.sh --shell "$shlist" ::: \
+            $(ls ./t-*.sh | grep -v "$LIB")
+    else
+        # ignore quotes
+        # shellcheck disable=SC2010,SC2046
+
+        ${test:+echo} ./run.sh --shell "$shlist" $(ls ./t-*.sh | grep -v "$LIB")
+    fi
+}
+
+{ Main "$@" ; } 2>&1
 
 # End of file
