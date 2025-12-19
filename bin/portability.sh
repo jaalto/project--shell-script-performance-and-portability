fc1a1dd368b54bf11939893ee8ea3f5db1824c54
Jari Aalto <jari.aalto@cante.net>
2025-12-19 12:05:50 +0200

    portability.sh: shellcheck

--- bin/portability.sh
+++ bin/portability.sh
@@ -26,7 +26,9 @@
 set -o errexit # Exit on error
 set -o nounset # Treat unused variables as errors
 
-VERSION="2025.1219.0925"
+# is used
+# shellcheck disable=SC2034
+VERSION="2025.1219.1002"
 
 PROGRAM=${0##*/}
 pwd=$(cd "$(dirname "$0")" && pwd)
