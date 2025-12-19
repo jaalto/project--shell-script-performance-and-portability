2512546c25ee59dc930f3ac70ce1984680696348
Jari Aalto <jari.aalto@cante.net>
2025-12-19 12:06:14 +0200

    run.sh: shellcheck

--- bin/run.sh
+++ bin/run.sh
@@ -26,7 +26,9 @@
 set -o errexit # Exit on error
 set -o nounset # Treat unused variables as errors
 
-VERSION="2025.1219.0921"
+# is used
+# shellcheck disable=SC2034
+VERSION="2025.1219.1003"
 
 PROGRAM=${0##*/}
 pwd=$(cd "$(dirname "$0")" && pwd)
