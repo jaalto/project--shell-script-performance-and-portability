9155a13efa1ac384fdbcbad01205968c1ff85715
Jari Aalto <jari.aalto@cante.net>
2025-12-23 11:24:03 +0200

    x-portability-statement-local-ifs.sh: adjust

--- bin/x-portability-statement-local-ifs.sh
+++ bin/x-portability-statement-local-ifs.sh
@@ -2,13 +2,13 @@
 # Short: local IFS
 # Desc: Test 'local' keyword with IFS in functions
 
-IFS="1"
-
 fn ()
 {
-    local IFS=""  # Change temporarily
+    local IFS
+    IFS=""
 }
 
+IFS=1
 fn
 
 [ "$IFS" = 1 ]
